6db1a49873c4b3acffc264cad8929e8df4c706ed
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-02-18 11:02:18 +0100

    ajout des Interfaces

---
+++ connectin-api/app/Interfaces/PostRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces;
+
+interface PostRepositoryInterface 
+{
+   public function all();                         // READ (tous)
+    public function find(int $id);                // READ (un seul)
+    public function create(array $data);           // CREATE
+    public function update(int $id, array $data);  // UPDATE
+    public function delete(int $id);               // DELETE
+}
