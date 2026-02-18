7d4ec6b2e33755a7a49fbd6dfe3da1f862ffdc0b
mina-barry <aminata.barry@epitech.eu>
2026-02-18 14:12:05 +0100

    Amelioration de Provider

--- connectin-api/app/Providers/AppServiceProvider.php
+++ connectin-api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
