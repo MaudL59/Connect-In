f0d51695862622f7ae78134907f1cfe0dcabed24
Maud Lecoeuvre <maud.lecoeuvre@epitech.eu>
2026-02-24 16:38:54 +0100

    ajout de la barre de recherche non fonctionnel et d'un bouton post non fonctionnel

--- connectin-api/resources/js/Accueil.jsx
+++ connectin-api/resources/js/Accueil.jsx
@@ -3,26 +3,41 @@ import React, { useState } from "react";
 
 export default function Accueil({ navigation, user }) {
     return (
-        <h1 className="h-20 text-white bg-blue-800 flex justify-around items-center text-xl font-semibold  w-full">
-            {" "}
-            <span
-                onClick={() => navigation("profil")}
-                className="cursor-pointer hover:underline"
-            >
-                Bienvenue, {user.first_name} {user.last_name} !
-            </span>
-            <span
-                onClick={() => navigation("accueil")}
-                className="cursor-pointer hover:underline"
-            >
-                CONNECT'IN
-            </span>
-            <bouton
-                onClick={() => navigation("login")}
-                className="cursor-pointer hover:underline"
-            >
-                Se déconnecter
-            </bouton>
-        </h1>
+        <div className="min-h-screen bg-slate-950 flex flex-col items-center">
+            {/* Le Header */}
+            <h1 className="h-20 text-white bg-blue-800 flex justify-around items-center text-xl font-semibold w-full">
+                <span
+                    onClick={() => navigation("profil")}
+                    className="cursor-pointer hover:underline"
+                >
+                    Bienvenue, {user.first_name} {user.last_name} !
+                </span>
+                <span
+                    onClick={() => navigation("accueil")}
+                    className="cursor-pointer hover:underline"
+                >
+                    CONNECT'IN
+                </span>
+                <button
+                    onClick={() => navigation("login")}
+                    className="cursor-pointer hover:underline"
+                >
+                    Se déconnecter
+                </button>
+            </h1>
+
+            {/* Le contenu principal */}
+            <main className="w-full max-w-2xl mt-8 px-4 flex flex-col gap-6">
+                <div className="flex flex-row gap-4 items-center">
+                    <label className="text-white">Recherche</label>
+                    <input
+                        className="flex-1 rounded-full px-4 py-2 text-black bg-white"
+                        placeholder="RECHERCHE UTILISATEUR"
+                        type="search"
+                    />
+                    <button className="bg-white rounded">CRÉER UN POST</button>
+                </div>
+            </main>
+        </div>
     );
 }
