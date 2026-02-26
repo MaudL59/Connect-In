1f712bc2c62815d2502c7dbc6cbd11469f0307f1
mina-barry <aminata.barry@epitech.eu>
2026-02-26 12:15:43 +0100

    ajout de la persistance d'actualisation de la page d'accueil

--- connectin-api/resources/js/app.jsx
+++ connectin-api/resources/js/app.jsx
@@ -1,6 +1,6 @@
 import "./bootstrap";
 import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react"; // Ajoute useEffect ici
 import React, { useState } from "react";
 import Login from "./Login";
 import Inscription from "./Inscription";
@@ -8,13 +8,26 @@ import Profil from "./Profil";
 import Accueil from "./Accueil";
 
 export default function App() {
-    const [page, setPage] = useState("login");
+    //  On vérifie si un token existe déjà dans le navigateur
+    const tokenExiste = localStorage.getItem("access_token");
+
+    //  Si le token existe, on commence sur "accueil", sinon sur "login"
+    const [page, setPage] = useState(tokenExiste ? "accueil" : "login");
+    
     const [user, setUser] = useState({
         last_name: "",
         first_name: "",
         email: "",
-        password: "",
     });
+
+    //   On peut aussi charger les infos de l'utilisateur stockées
+    useEffect(() => {
+        const savedUser = localStorage.getItem("user_data");
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
+
     const pages = {
         login: <Login navigation={setPage} setUser={setUser} />,
         inscription: <Inscription navigation={setPage} setUser={setUser} />,
