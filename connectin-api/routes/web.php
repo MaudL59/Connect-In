9316fd382a9548e614f04d86bb780c50452a3299
Maud Lecoeuvre <maud.lecoeuvre@epitech.eu>
2026-03-05 11:33:10 +0100

    changement url

--- connectin-api/routes/web.php
+++ connectin-api/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// route vers welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route pour que url change en changeant de page
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
